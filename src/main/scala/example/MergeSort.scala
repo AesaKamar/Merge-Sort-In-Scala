25b72decf3322474ecd163296aea3e8e302a0763
AesaKamar <aesa.kamar@gmail.com>
2017-08-24 13:46:04 -0400

    02 Data Structure Definition

--- src/main/scala/example/MergeSort.scala
+++ src/main/scala/example/MergeSort.scala
@@ -3,7 +3,9 @@ package example
 /**
  * Define our Data Structure
  */
-
+sealed trait SortableList[Int]
+case class SortedList[Int](x: List[Int]) extends SortableList[Int]
+case class UnsortedList[Int](x: List[Int]) extends SortableList[Int]
 
 /**
  * Define our Algorithm

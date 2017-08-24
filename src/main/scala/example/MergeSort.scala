e12596f7560f9b00ae349acd95cecd0d527043aa
AesaKamar <aesa.kamar@gmail.com>
2017-08-24 13:54:26 -0400

    05 Case Destructuring

--- src/main/scala/example/MergeSort.scala
+++ src/main/scala/example/MergeSort.scala
@@ -1,15 +1,19 @@
 package example
 
 /**
- * Define our Data Structure
- */
+  * Define our Data Structure
+  */
 sealed trait SortableList[Int]
 case class SortedList[Int](x: List[Int]) extends SortableList[Int]
 case class UnsortedList[Int](x: List[Int]) extends SortableList[Int]
 
 /**
- * Define our Algorithm
- */
+  * Define our Algorithm
+  */
 object MergeSort {
-  def MergeSort(in: SortableList[Int]) : SortedList[Int] = ???
+  def MergeSort(in: SortableList[Int]): SortedList[Int] =
+    in match {
+      case SortedList(x) => SortedList(x)
+      case UnsortedList(x) => ???
+    }
 }
